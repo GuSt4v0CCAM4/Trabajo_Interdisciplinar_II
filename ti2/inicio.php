d5a90ff9dec2a1fee4bc91d06e5cb4d1b3e606bb
Gustavo Ccama Marron <104206033+GuSt4v0CCAM4@users.noreply.github.com>
2022-09-26 20:27:04 -0500

    Add files via upload

---
+++ ti2/inicio.php
@@ -0,0 +1,134 @@
+    <!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Capacitaciones</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="Shortcut Icon" type="image/x-icon" href="assets/icons/logoo.png" />
+    <script src="js/sweet-alert.min.js"></script>
+    <link rel="stylesheet" href="css/sweet-alert.css">
+    <link rel="stylesheet" href="css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script src="js/modernizr.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <div class="navbar-lateral full-reset">
+        <div class="visible-xs font-movile-menu mobile-menu-button"></div>
+        <div class="full-reset container-menu-movile custom-scroll-containers">
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+        	<br>
+            <div class="full-reset" style="background-color:#2B3D51; padding: 10px 0; color:#fff;">
+                <figure>
+                    <img src="assets/img/logoo.png" alt="Biblioteca" class="img-responsive center-box" style="width:55%;">
+                </figure>
+                <p class="text-center" style="padding-top: 15px;">Capacitaciones Empresariales</p>
+            </div>
+            <div class="full-reset nav-lateral-list-menu">
+                <ul class="list-unstyled">
+                    <li><a href="esperainicio.php"><i class="zmdi zmdi-home zmdi-hc-fw"></i>&nbsp;&nbsp; Inicio</a></li>
+                    <li>
+                        <div class="dropdown-menu-button"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp; Capacitaciones Disponibles <i class="zmdi zmdi-chevron-down pull-right zmdi-hc-fw"></i></div>
+                        <ul class="list-unstyled">
+                           <li><a href="esperaacción.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Capacitación de onboarding</a></li>
+                            <li><a href="esperacienciaficción.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Capacitación de cumplimiento normativo </a></li>
+                            <li><a href="esperacomedia.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Capacitación de habilidades técnicas </a></li>
+                            <li><a href="esperafantasía.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Capacitación de habilidades blandas</a></li>
+                            <li><a href="esperamelodrama.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Capacitación de productos o servicios</a></li>
+                            <li><a href="esperamusical.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Capacitación en liderazgo</a></li>
+                            <li><a href="esperaromance.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Capacitación en seguridad en el trabajo</a></li>
+                            <li><a href="esperasuspense.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Suspense</a></li>
+                            <li><a href="esperaterror.php"><i class="zmdi zmdi-movie-alt"></i>&nbsp;&nbsp; Terror </a></li>
+                        </ul>
+                    </li>
+                    	 <li><a href="espera.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Cuestionarios </a></li>
+
+                    	 <li><a href="desconectar.php"><i class="zmdi zmdi-movie"></i>&nbsp;&nbsp;  Cerrar</a></li>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="content-page-container full-reset custom-scroll-containers">
+
+    	 <div class="footer-copyright full-reset all-tittles">GRUPO 3 - TRABAJO INTERDISICPLINAR II</div>
+
+        <div class="container">
+            <div class="page-header">
+              <h1 class="all-tittles"><center><font size="10" face="Algerian">CAPACITACIONES</font></center></h1>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <ul class="nav nav-tabs nav-justified"  style="font-size: 17px;">
+
+            </ul>
+        </div>
+        <div class="container-fluid"  style="margin: 50px 0;">
+            <div class="row">
+                <div class="col-xs-12 col-sm-4 col-md-3">
+                    <img src="assets/img/empresa.png" alt="user" class="img-responsive center-box" style="max-width: 110px;">
+                </div>
+                <div class="col-xs-12 col-sm-8 col-md-8 text-justify lead">
+                    <p>Para cualquier empresa, es muy importante implementar capacitaciones que empoderen en habilidades y conocimientos a sus empleados. Estas son tan imprescindibles como la adecuada selección y contratación del mejor talento.De hecho, según un estudio compartido por Team Stage, las compañías que invierten en capacitaciones obtienen un 24% más de rentabilidad. Además, aquellas que cuentan con programas de capacitación bien diseñados experimentan una retención del empleado de 30 a 50%. Son cifras bastante potentes, ¿verdad? Por esa razón, es necesario que sepas cuáles son los tipos de capacitación en una empresa. En este artículo, te explicamos los 7 tipos más importantes y cuándo implementar cada uno.</p>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+<div class="container-flat-form">
+<br>
+<br>
+<a href="espera.php">
+<img src="assets/img/alien.png" alt="Biblioteca" class="img-responsive center-box" style="width:70%;">
+</a>
+<br>
+<br>
+</div>
+
+<div class="container-flat-form">
+<br>
+<br>
+<a href="espera.php">
+<img src="assets/img/transformers.png" alt="Biblioteca" class="img-responsive center-box" style="width:70%;">
+</a>
+<br>
+<br>
+</div>
+
+<div class="container-flat-form">
+<br>
+<br>
+<a href="espera.php">
+<img src="assets/img/logan.png" alt="Biblioteca" class="img-responsive center-box" style="width:70%;">
+</a>
+<br>
+<br>
+</div>
+
+<div class="container-flat-form">
+<br>
+<br>
+<a href="espera.php">
+<img src="assets/img/guerrapapas2.png" alt="Biblioteca" class="img-responsive center-box" style="width:70%;">
+</a>
+<br>
+<br>
+</div>
+
+        </div>
+
+    </div>
+</body>
+</html>
